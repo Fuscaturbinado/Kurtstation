3cbe76be904c87047f4eed0cf73b902920f929dc
lorez <125885188+Fuscaturbinado@users.noreply.github.com>
2024-08-01 23:26:35 -0300

    Add files via upload

---
+++ Siteette/but.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test dropdown</title>
+    <link rel="stylesheet" href="drop.css">
+</head>
+<body>
+ 
+        <nav>
+
+            <ul>
+                <li class="dropdown">
+                    <a href="#">casa</a>
+                    <div class="dropdown-menu">
+                    <a href="#">test</a>
+                    <a href="#">test4</a>
+                    <a href="#">test25</a>
+
+    
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <a href="#">alog</a>
+                    <div class="dropdown-menu">
+                         <a href="#">test</a>
+                         <a href="#">test1</a>
+                         <a href="#">test2</a>
+                    </div>
+                </li>
+                <li>
+                    <a href="#">saaa</a>
+                </li>
+                <li>
+                    <a href="#">aleweog</a>
+                </li>
+            </ul>
+
+        </nav>
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+</body>
+</html>
---
+++ Siteette/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html rel="stylesheet" lang="en">
+<head>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Playwrite+BE+VLG:wght@100..400&display=swap');
+</style>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="estilo.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Own Site</title>
+    <script src="functions.js" defer></script>
+</head>
+
+<body class="theme">
+<nav>
+    <ul>
+        <div class="titulop">
+        <label class="h1d">Kunst</label>
+        <label class="subtitulo" >station</label>
+        </div>
+        <li class="dropdown">
+            <a href="#">About the Site</a>
+            <div class="dropdown-menu">
+            <a href="#">Criador</a>
+            <a href="#">Propósito</a>
+            <a href="#">sei lá</a>
+            </div>
+        </li><li class="dropdown">
+            <a href="#">Arts</a>
+            <div class="dropdown-menu">
+                 <a href="#">test</a>
+                 <a href="#">test1</a>
+                 <a href="#">test2</a>
+            </div>
+        </li><li class="dropdown">
+            <a href="#">Acounts</a>
+            <div class="dropdown-menu">
+            <a href="#">test</a>
+            <a href="#">test1</a>
+            <a href="#">test2</a>
+            </div>
+        </li><li class="dropdown">
+            <a href="#">Videos trailers</a>
+            <div class="dropdown-menu">
+            <a href="#">test</a>
+            <a href="#">test1</a>
+            <a href="#">test2</a>
+            <a href="#">test</a>
+            <a href="#">test1</a>
+            <a href="#">test2</a>
+
+            </div>
+        </li>
+    </ul>
+</nav>
+<div>
+
+
+</div>
+
+
+</body>
+</html>
