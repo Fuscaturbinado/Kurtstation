858290a6b835ed521cd29e66569dda647f5efedd
lorez <lolitomg07@gmail.com>
2024-09-11 23:36:57 -0300

    ll

--- Siteette/acao.php
+++ Siteette/acao.php
@@ -1,35 +1,14 @@
 <?php
 include_once ('database.php');
 
-/*if (isset($_POST['submit'])) {
- $file= $_FILES['file'];
-$filename = $_FILES['file']['name'];
-$tempname = $_FILES['file']['tempname'];
 
-$query = mysqli_query($sql, "insert into img (file) valures('$filename')");
-if(move_uploaded_file($tempname, )){}
-}else{ die ;}
-*/
-if (isset($_FILES['image']) && $_FILES['image']['error'] == UPLOAD_ERR_OK) {
-    $imageData = file_get_contents($_FILES['image']['tmp_name']);
-    $imageType = $_FILES['image']['type'];
 
-    // Prepare and execute the SQL query
-    $stmt = $sql->prepare("INSERT INTO post (image_data, image_type) VALUES (?, ?)");
-    $stmt->bind_param("bs", $imageData, $imageType);
-    $stmt->send_long_data(0, $imageData); // Bind the long data for the BLOB
-    
-    if ($stmt->execute()) {
-        echo "Image uploaded successfully.";
-    } else {
-        echo "Error: " . $stmt->error;
-    }
+$imagem = $_FILES["imagem"];
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "kurst";
 
-    // Close the statement and connection
-    $stmt->close();
-} else {
-    echo "No file uploaded or there was an upload error.";
-}
 
-$sql->close();
+
 ?>
--- Siteette/postcreate.php
+++ Siteette/postcreate.php
@@ -8,12 +8,24 @@
 </head>
 <body>
 
-    <form action="acao.php" method="POST" enctype="multipart/form-data">
-<label>Title</label>
-<input type="text" required>
-<input type="file" name="image" accept=".jpg , .jpeg, .png ">
-<button type="submit">UPLOAD</button>
+<title>Upload de imagens com PHP</title>
+		<meta charset="utf-8"/>
+</head>
+<body>
+	<form action="gravar.php" method="POST" enctype="multipart/form-data">
+		<label for="imagem">Imagem:</label>
+		<input type="file" name="imagem"/>
+		<br/>
+		<input type="submit" value="Enviar"/>
+	</form>
 
+<form action="acao.php">
+    <p>Titulo</p>
+    <input type="text" name="Titulo">
+    <P>Subtexto</P>
+    <input type="text" name="sutext">
+    <input type="submit">
 </form>
+
 </body>
 </html>
